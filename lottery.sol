9ed9fa167abcf9db189d65e9ccd44541bcf26e67
hisahito <hisahito706@gmail.com>
2022-07-26 17:06:29 -0500

    creating our first function in a smart contract

--- lottery.sol
+++ lottery.sol
@@ -2,12 +2,18 @@
 
 pragma solidity ^0.8.7;
 
-contract lottery{
+contract Lottery{
     uint public minFee;
     address[] players;
 
     constructor(uint _minFee){
         minFee = _minFee;
     }
-}
+
+    function play() public{
+        require(msg.value >= minFee);
+        players.push(msg.sender);
+    }
+} 
+
 
